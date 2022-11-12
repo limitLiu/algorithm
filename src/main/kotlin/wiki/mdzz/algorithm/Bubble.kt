40283268bf1083c9b50bb3654d539eb3b3364273
limit <limitliu@qq.com>
2022-11-13 00:40:00 +0800

    feat(Bubble.kt): bubble sort

--- src/main/kotlin/wiki/mdzz/algorithm/Bubble.kt
+++ src/main/kotlin/wiki/mdzz/algorithm/Bubble.kt
@@ -4,7 +4,7 @@ import wiki.mdzz.utils.list.Vec
 
 class Bubble {
     companion object {
-        fun <T : Comparable<T>> sort(vec: Vec<T>): Vec<T> {
+        fun <T : Comparable<T>> sort1(vec: Vec<T>): Vec<T> {
             for (end in vec.size() - 1 downTo 1) {
                 for (i in 1..end) {
                     if (vec[i]!! < vec[i - 1]!!) {
@@ -16,5 +16,40 @@ class Bubble {
             }
             return vec
         }
+
+        fun <T : Comparable<T>> sort2(vec: Vec<T>): Vec<T> {
+            for (end in vec.size() - 1 downTo 1) {
+                var sorted = true
+                for (i in 1..end) {
+                    if (vec[i]!! < vec[i - 1]!!) {
+                        val temp = vec[i]
+                        vec[i] = vec[i - 1]
+                        vec[i - 1] = temp
+                        sorted = false
+                    }
+                }
+                if (sorted) break
+            }
+            return vec
+        }
+
+        fun <T : Comparable<T>> sort3(vec: Vec<T>): Vec<T> {
+            var end: Int = vec.size() - 1
+            while (end > 0) {
+                var index = 1
+                for (i in 1..end) {
+                    if (vec[i]!! < vec[i - 1]!!) {
+                        val temp = vec[i]
+                        vec[i] = vec[i - 1]
+                        vec[i - 1] = temp
+                        index = i
+                    }
+                }
+                end = index
+                end--
+            }
+            return vec
+        }
+
     }
 }
--- src/test/kotlin/algorithm/SortTest.kt
+++ src/test/kotlin/algorithm/SortTest.kt
@@ -8,12 +8,28 @@ import wiki.mdzz.utils.list.Vec
 class SortTest {
     @Test
     fun bubbleTest() {
-        val vec = Vec.arrayOf(35, 1, 3, 66, 5)
-        val newVec = Bubble.sort(vec)
-        Assert.assertTrue(newVec[0] == 1)
-        Assert.assertTrue(newVec[1] == 3)
-        Assert.assertTrue(newVec[2] == 5)
-        Assert.assertTrue(newVec[3] == 35)
-        Assert.assertTrue(newVec[4] == 66)
+        val vec0 = Vec.arrayOf(35, 1, 3, 66, 5)
+        val newVec0 = Bubble.sort1(vec0)
+        Assert.assertTrue(newVec0[0] == 1)
+        Assert.assertTrue(newVec0[1] == 3)
+        Assert.assertTrue(newVec0[2] == 5)
+        Assert.assertTrue(newVec0[3] == 35)
+        Assert.assertTrue(newVec0[4] == 66)
+
+        val vec1 = Vec.arrayOf(35, 1, 3, 66, 5)
+        val newVec1 = Bubble.sort2(vec1)
+        Assert.assertTrue(newVec1[0] == 1)
+        Assert.assertTrue(newVec1[1] == 3)
+        Assert.assertTrue(newVec1[2] == 5)
+        Assert.assertTrue(newVec1[3] == 35)
+        Assert.assertTrue(newVec1[4] == 66)
+
+        val vec2 = Vec.arrayOf(35, 1, 3, 66, 5)
+        val newVec2 = Bubble.sort3(vec2)
+        Assert.assertTrue(newVec2[0] == 1)
+        Assert.assertTrue(newVec2[1] == 3)
+        Assert.assertTrue(newVec2[2] == 5)
+        Assert.assertTrue(newVec2[3] == 35)
+        Assert.assertTrue(newVec2[4] == 66)
     }
 }
